bf4d1828a42f51b85034dd502e51932af198df9c
Pablo Hoffman <pablo@pablohoffman.com>
2013-05-21 18:19:42 -0300

    reafactor tests:
    - make them faster by spawning servers once (not on every test)
    - shared common tests for common functionality (error checks and such)

--- splash2/tests/test_render.py
+++ splash2/tests/test_render.py
@@ -1,49 +1,65 @@
 import unittest, imghdr, requests
 from splash2.tests.utils import TestServers
 
-class RenderHtmlTest(unittest.TestCase):
+class _RenderTest(unittest.TestCase):
 
-    def test_ok(self):
-        with TestServers():
-            r = requests.get("http://localhost:8050/render.html?url=http://localhost:8998/jsrender")
-            self.assertEqual(r.status_code, 200)
-            self.assertEqual(r.headers["content-type"].lower(), "text/html; charset=utf-8")
-            self.assertTrue("Before" not in r.text)
-            self.assertTrue("After" in r.text)
+    host = "localhost:8050"
+    render_format = "html"
+
+    def request(self, query):
+        url = "http://%s/render.%s?%s" % (self.host, self.render_format, query)
+        return requests.get(url)
 
     def test_render_error(self):
-        with TestServers():
-            r = requests.get("http://localhost:8050/render.html?url=http://non-existent-host/")
-            self.assertEqual(r.status_code, 502)
+        r = self.request("url=http://non-existent-host/")
+        self.assertEqual(r.status_code, 502)
 
     def test_timeout(self):
-        with TestServers():
-            r = requests.get("http://localhost:8050/render.html?url=http://localhost:8998/delay?n=10&timeout=0.5")
-            self.assertEqual(r.status_code, 504)
+        r = self.request("url=http://localhost:8998/delay?n=10&timeout=0.5")
+        self.assertEqual(r.status_code, 504)
 
     def test_missing_url(self):
-        with TestServers():
-            r = requests.get("http://localhost:8050/render.html")
-            self.assertEqual(r.status_code, 400)
-            self.assertTrue("url" in r.text)
+        r = self.request("")
+        self.assertEqual(r.status_code, 400)
+        self.assertTrue("url" in r.text)
 
-    def test_baseurl(self):
-        with TestServers():
-            # first make sure that script.js is served under the right url
-            self.assertEqual(404, requests.get("http://localhost:8998/script.js").status_code)
-            self.assertEqual(200, requests.get("http://localhost:8998/baseurl/script.js").status_code)
 
-            r = requests.get("http://localhost:8050/render.html?url=http://localhost:8998/baseurl&baseurl=http://localhost:8998/baseurl/")
-            self.assertEqual(r.status_code, 200)
-            self.assertTrue("Before" not in r.text)
-            self.assertTrue("After" in r.text)
+class RenderHtmlTest(_RenderTest):
 
-class RenderPngTest(unittest.TestCase):
+    render_format = "html"
 
     def test_ok(self):
-        with TestServers():
-            r = requests.get("http://localhost:8050/render.png?url=http://localhost:8998/jsrender")
-            self.assertEqual(r.status_code, 200)
-            self.assertEqual(r.headers["content-type"], "image/png")
-            self.assertGreater(len(r.content), 0)
-            self.assertEqual(imghdr.what("", r.content), "png")
+        r = self.request("url=http://localhost:8998/jsrender")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers["content-type"].lower(), "text/html; charset=utf-8")
+        self.assertTrue("Before" not in r.text)
+        self.assertTrue("After" in r.text)
+
+    def test_baseurl(self):
+        # first make sure that script.js is served under the right url
+        self.assertEqual(404, requests.get("http://localhost:8998/script.js").status_code)
+        self.assertEqual(200, requests.get("http://localhost:8998/baseurl/script.js").status_code)
+
+        r = self.request("url=http://localhost:8998/baseurl&baseurl=http://localhost:8998/baseurl/")
+        self.assertEqual(r.status_code, 200)
+        self.assertTrue("Before" not in r.text)
+        self.assertTrue("After" in r.text)
+
+class RenderPngTest(_RenderTest):
+
+    render_format = "png"
+
+    def test_ok(self):
+        r = self.request("url=http://localhost:8998/jsrender")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers["content-type"], "image/png")
+        self.assertGreater(len(r.content), 0)
+        self.assertEqual(imghdr.what("", r.content), "png")
+
+ts = TestServers()
+
+def setup():
+    ts.__enter__()
+
+def teardown():
+    ts.__exit__(None, None, None)
