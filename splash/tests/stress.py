7b04ead920fecc5b3511e67633b1b089ce483533
Pablo Hoffman <pablo@pablohoffman.com>
2013-06-06 22:20:56 -0300

    many improvements to stess testing script

--- splash/tests/stress.py
+++ splash/tests/stress.py
@@ -1,95 +1,138 @@
-import sys, requests, random, optparse, time
+import sys, requests, random, optparse, time, json
+from itertools import islice
 from Queue import Queue
 from threading import Thread
 from collections import Counter
-from tempfile import NamedTemporaryFile
-from splash.tests.utils import TestServers
 
 
 class StressTest():
+
+    def __init__(self, reqs, host="localhost:8050", requests=1000, concurrency=50, shuffle=False, verbose=False):
+        self.reqs = reqs
+        self.host = host
+        self.requests = requests
+        self.concurrency = concurrency
+        self.shuffle = shuffle
+        self.verbose = verbose
+
+    def run(self):
+        args = list(islice(self.reqs, self.requests))
+        if self.shuffle:
+            random.shuffle(args)
+        print "Total requests: %d" % len(args)
+        print "Concurrency   : %d" % self.concurrency
+
+        starttime = time.time()
+        q, p = Queue(), Queue()
+        for _ in xrange(self.concurrency):
+            t = Thread(target=worker, args=(self.host, q, p, self.verbose))
+            t.daemon = True
+            t.start()
+        for a in args:
+            q.put(a)
+        q.join()
+
+        outputs = []
+        for _ in xrange(self.requests):
+            outputs.append(p.get())
+
+        elapsed = time.time() - starttime
+        print
+        print "Total requests: %d" % len(args)
+        print "Concurrency   : %d" % self.concurrency
+        print "Elapsed time  : %.3fs" % elapsed
+        print "Avg time p/req: %.3fs" % (elapsed/len(args))
+        print "Received (per status code or error):"
+        for c, n in Counter(outputs).items():
+            print "  %s: %d" % (c, n)
+
+def worker(host, q, p, verbose=False):
+    url = "http://%s/render.html" % host
+    while True:
+        try:
+            args = q.get()
+            t = time.time()
+            r = requests.get(url, params=args)
+            t = time.time() -t
+            p.put(r.status_code)
+            if verbose:
+                print ". %.3fs %s" % (t, args)
+            else:
+                sys.stdout.write(".")
+                sys.stdout.flush()
+        except Exception as e:
+            p.put(type(e))
+            if verbose:
+                print "E %.3fs %s" % (t, args)
+            else:
+                sys.stdout.write("E")
+                sys.stdout.flush()
+        finally:
+            q.task_done()
+
+
+class MockArgs(object):
 
     ok_urls = 0.5
     error_urls = 0.3
     timeout_urls = 0.2
 
-    def __init__(self, total=1000, concurrency=50):
-        self.requests = total
-        self.concurrency = concurrency
-        
+    def __init__(self, requests=1000):
+        self.requests = requests
 
     def _ok_urls(self):
-        url = ["http://localhost:8050/render.html?url=http://localhost:8998/jsrender"]
+        url = ["http://localhost:8998/jsrender"]
         return int(self.requests * self.ok_urls) * url
 
     def _error_urls(self):
-        url = ["http://localhost:8050/render.html?url=http://non-existent-host/"]
+        url = ["http://non-existent-host/"]
         return int(self.requests * self.error_urls) * url
 
     def _timeout_urls(self):
-        url = ["http://localhost:8050/render.html?url=http://localhost:8998/delay?n=10&timeout=0.5"]
+        url = ["http://localhost:8998/delay?n=10&timeout=0.5"]
         return int(self.requests * self.timeout_urls) * url
 
-    def run(self):
-        f = NamedTemporaryFile(prefix="splash-stress-", suffix=".log", delete=False)
-        # we need to use log file to prevent deadlocks on heavy loads
-        with TestServers(logfile=f.name):
-            ok_urls = self._ok_urls()
-            error_urls = self._error_urls()
-            timeout_urls = self._timeout_urls()
-            urls = ok_urls + error_urls + timeout_urls
-            random.shuffle(urls)
+    def __iter__(self):
+        ok_urls = self._ok_urls()
+        error_urls = self._error_urls()
+        timeout_urls = self._timeout_urls()
+        urls = ok_urls + error_urls + timeout_urls
+        return ({"url": x} for x in urls)
 
-            print "Total requests: %d" % len(urls)
-            print "Concurrency   : %d" % self.concurrency
-            print "Log file      : %s" % f.name
 
-            starttime = time.time()
-            q, p = Queue(), Queue()
-            for _ in xrange(self.concurrency):
-                t = Thread(target=worker, args=(q, p))
-                t.daemon = True
-                t.start()
-            for url in urls:
-                q.put(url)
-            q.join()
+class ArgsFromUrlFile(object):
 
-            outputs = []
-            for _ in xrange(self.requests):
-                outputs.append(p.get())
+    def __init__(self, urlfile):
+        self.urlfile = urlfile
 
-            elapsed = time.time() - starttime
-            expected = {
-                200: len(ok_urls),
-                502: len(error_urls),
-                504: len(timeout_urls),
-            }
-            print
-            print "Total requests: %d" % len(urls)
-            print "Concurrency   : %d" % self.concurrency
-            print "Log file      : %s" % f.name
-            print "Elapsed time  : %.3fs" % elapsed
-            print "Avg time p/req: %.3fs" % (elapsed/len(urls))
-            print "Received/Expected (per status code or error):"
-            for c, n in Counter(outputs).items():
-                print "  %s: %d/%d" % (c, n, expected.get(c, 0))
+    def __iter__(self):
+        for l in open(self.urlfile):
+            yield {"url": l.rstrip()}
 
-def worker(q, p):
-    while True:
-        try:
-            url = q.get()
-            r = requests.get(url)
-            p.put(r.status_code)
-            sys.stdout.write(".")
-            sys.stdout.flush()
-        except Exception as e:
-            p.put(type(e))
-            sys.stdout.write("E")
-            sys.stdout.flush()
-        finally:
-            q.task_done()
+class ArgsFromLogfile(object):
+
+    def __init__(self, logfile):
+        self.logfile = logfile
+
+    def __iter__(self):
+        for l in open(self.logfile):
+            if "[stats]" in l:
+                d = json.loads(l[33:].rstrip())
+                yield d['args']
+
 
 def parse_opts():
     op = optparse.OptionParser()
+    op.add_option("-H", dest="host", default="localhost:8050",
+            help="splash hostname & port (default: %default)")
+    op.add_option("-u", dest="urlfile", metavar="FILE",
+            help="read urls from FILE instead of using mock server ones")
+    op.add_option("-l", dest="logfile", metavar="FILE",
+            help="read urls from splash log file (useful for replaying)")
+    op.add_option("-s", dest="shuffle", action="store_true", default=False,
+            help="shuffle (randomize) requests (default: %default)")
+    op.add_option("-v", dest="verbose", action="store_true", default=False,
+            help="verbose mode (default: %default)")
     op.add_option("-c", dest="concurrency", type="int", default=50,
             help="concurrency (default: %default)")
     op.add_option("-n", dest="requests", type="int", default=1000,
@@ -98,7 +141,13 @@ def parse_opts():
 
 def main():
     opts, _ = parse_opts()
-    t = StressTest(opts.requests, opts.concurrency)
+    if opts.urlfile:
+        urls = ArgsFromUrlFile(opts.urlfile)
+    elif opts.logfile:
+        urls = ArgsFromLogfile(opts.logfile)
+    else:
+        urls = MockArgs(opts.requests)
+    t = StressTest(urls, opts.host, opts.requests, opts.concurrency, opts.shuffle, opts.verbose)
     t.run()
 
 if __name__ == "__main__":
