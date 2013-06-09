2dc1cf0ef0ba0d94c948c41f67aabebd2bb93885
Pablo Hoffman <pablo@pablohoffman.com>
2013-06-09 11:46:06 -0300

    ensure there are no leaks, in tests. disabled for now because they fail

--- splash/tests/test_render.py
+++ splash/tests/test_render.py
@@ -92,4 +92,6 @@ def setup():
     ts.__enter__()
 
 def teardown():
+    #leaks = requests.get("http://localhost:8050/debug").json()['leaks']
+    #assert not leaks, "Leaks detected:\n%s" % leaks
     ts.__exit__(None, None, None)
