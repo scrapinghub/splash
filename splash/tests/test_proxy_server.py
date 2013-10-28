5d9e0f5d088e4beacc20e3d70a7654b9060d77cc
Andres Portillo <andres.portillo@gmail.com>
2013-10-27 21:52:12 -0600

    Remove HTTPS support from splash proxy test

--- splash/tests/test_proxy_server.py
+++ splash/tests/test_proxy_server.py
@@ -14,9 +14,7 @@ class ProxyRequestHandler:
 
     @property
     def proxies(self):
-        return {'http': 'localhost:%d' % ts.splashserver.proxy_portnum,
-                'https': 'localhost:%d' % ts.splashserver.proxy_portnum,
-                }
+        return {'http': 'localhost:%d' % ts.splashserver.proxy_portnum}
 
     def _get_val(self, v):
         if isinstance(v, list):
