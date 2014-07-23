eda792a48819b430e8f6b4bbe467cd3f3c87dfa2
Mikhail Korobov <kmike84@gmail.com>
2014-07-24 02:56:08 +0600

    Drop Python 2.6 support and document PyQT version Splash is tested on. Closes GH-104.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup_args = {
     'scripts': ['bin/splash'],
     'classifiers': [
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
