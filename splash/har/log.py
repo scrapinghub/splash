23c69c6e75bc5fa774576ed0af01d02c731262b3
Mikhail Korobov <kmike84@gmail.com>
2014-10-03 03:23:39 +0600

    remove unneeded import

--- splash/har/log.py
+++ splash/har/log.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from collections import namedtuple, Counter
+from collections import namedtuple
 from datetime import datetime
 
 import splash
