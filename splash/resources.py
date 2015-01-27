de93746525e1f92e1bfe3112ed4915fc6dd54497
Mikhail Korobov <kmike84@gmail.com>
2015-01-27 15:45:54 +0500

    simplify the default example script

--- splash/resources.py
+++ splash/resources.py
@@ -583,29 +583,11 @@ class Root(Resource):
 
     def get_example_script(self):
         return """
--- Hey, this is a Splash rendering script.
--- It is written in Lua. Hope you like it.
-
 function main(splash)
-
   local url = splash.args.url
-
-  local ok, msg = splash:go(url) -- this is async!
-  if not ok then
-    return {status="error", msg=msg}
-  end
-
-  splash:wait(0.5)
-  splash:stop()
-
-  local prefixed_title = splash:jsfunc([[
-    function(prefix){
-      return prefix + " " + document.title;
-    }
-  ]])
-
+  assert(splash:go(url))
+  assert(splash:wait(0.5))
   return {
-    greeting = prefixed_title("Hello, "),
     html = splash:html(),
     png = splash:png{width=640},
     har = splash:har(),
