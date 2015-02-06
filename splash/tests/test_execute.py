b435450c4a500adf082a6888866809330232f251
Mikhail Korobov <kmike84@gmail.com>
2015-02-06 06:31:35 +0500

    Test for unicode handling in Lua JSON POST requests. See GH-174.

--- splash/tests/test_execute.py
+++ splash/tests/test_execute.py
@@ -888,6 +888,19 @@ class ArgsTest(BaseLuaRenderTest):
         self.assertIn("Invalid filter names", resp.text)
 
 
+class JsonPostUnicodeTest(BaseLuaRenderTest):
+    request_handler = JsonPostRequestHandler
+
+    def test_unicode(self):
+        resp = self.request_lua(u"""
+        function main(splash) return {key="значение"} end
+        """.encode('utf8'))
+
+        self.assertStatusCode(resp, 200)
+        self.assertEqual(resp.headers['content-type'], 'application/json')
+        self.assertEqual(resp.json(), {"key": u"значение"})
+
+
 class JsonPostArgsTest(ArgsTest):
     request_handler = JsonPostRequestHandler
 
