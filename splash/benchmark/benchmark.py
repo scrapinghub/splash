9b418b775609162d0d8c399075316a0e8272b4ad
immerrr <immerrr@gmail.com>
2015-02-28 19:20:20 +0300

    benchmark: print some metrics

--- splash/benchmark/benchmark.py
+++ splash/benchmark/benchmark.py
@@ -9,22 +9,33 @@ server and runs a series of requests via splash on those downloaded pages.
 """
 
 import logging
+import os
 import random
+import shutil
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from glob import glob
 from multiprocessing.pool import ThreadPool
+from pprint import pformat
+from time import time
 
 import requests
 from splash.benchmark.file_server import serve_files
 from splash.tests.utils import SplashServer
 
+#: Port at which static pages will be served.
 PORT = 8806
-#: URLs to benchmark against.
+#: Static pages to be used in the benchmark.
 PAGES = glob('localhost_8806/*.html')
 #: Combinations of width & height to test.
 WIDTH_HEIGHT = [(None, None), (500, None), (None, 500), (500, 500)]
 # XXX: add benchmark of different API endpoints.
 SPLASH_LOG = 'splash.log'
+#: This script is used to collect maxrss & cpu time from splash process.
+GET_PERF_STATS_SCRIPT = """
+function main(splash)
+  return splash:get_perf_stats()
+end
+"""
 
 parser = ArgumentParser(description=__doc__,
                         formatter_class=ArgumentDefaultsHelpFormatter)
@@ -60,20 +71,42 @@ def invoke_request(invoke_args):
     log = logging.getLogger('bench_worker')
     req_no, total_reqs, kwargs = invoke_args
     log.info("Initiating request %d/%d: %s", req_no, total_reqs, kwargs)
-    return requests.get(**kwargs)
+    stime = time()
+    requests.get(**kwargs)
+    etime = time()
+    return {'start_time': stime,
+            'end_time': etime,
+            'duration': etime - stime,
+            'endpoint': kwargs['url'],
+            'site': kwargs['params']['url'],
+            'width': kwargs['params']['width'],
+            'height': kwargs['params']['height']}
 
 
 def main():
+    log = logging.getLogger("benchmark")
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG)
 
-    with SplashServer(logfile=SPLASH_LOG,
-                      extra_args=['--disable-lua-sandbox',
-                                  '--disable-xvfb',
-                                  '--max-timeout=600']) as splash, \
-         serve_files(PORT):
-        parallel_map(invoke_request, generate_requests(splash, args),
-                     args.thread_count)
+    splash = SplashServer(
+        logfile=SPLASH_LOG,
+        extra_args=['--disable-lua-sandbox',
+                    '--disable-xvfb',
+                    '--max-timeout=600'])
+
+    with splash, serve_files(PORT):
+        start_time = time()
+        results = parallel_map(invoke_request, generate_requests(splash, args),
+                               args.thread_count)
+        end_time = time()
+        resources = requests.get(
+            splash.url('execute'),
+            params={'lua_source': GET_PERF_STATS_SCRIPT}).json()
+
+    log.info("Request stats:\n%s", pformat(dict(enumerate(results))))
+    log.info("Splash max RSS: %s B", resources['maxrss'])
+    log.info("Splash CPU time elapsed: %.2f sec", resources['cputime'])
+    log.info("Wallclock time elapsed: %.2f sec", end_time - start_time)
 
 
 if __name__ == '__main__':
