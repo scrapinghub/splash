c89b11e57fbb33869e4f446cbb354d6b1ecc2e21
David Bengoa <david@bengoarocandio.com>
2015-06-05 21:16:18 +0700

    Use fnmatch to match content types

--- splash/response_middleware.py
+++ splash/response_middleware.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 from PyQt4.QtNetwork import QNetworkRequest
 from splash.qtutils import request_repr
 from twisted.python import log
+import fnmatch
 
 class ContentTypeMiddleware(object):
     """
@@ -21,38 +22,34 @@ class ContentTypeMiddleware(object):
 
     @staticmethod
     def contains(mime_set, mime):
-        for part1 in mime[0], '*':
-            for part2 in mime[1], '*':
-                if (part1, part2) in mime_set:
-                    return True
+        for pattern in mime_set:
+            if fnmatch.fnmatch(mime, pattern):
+                return True
         return False
 
     @staticmethod
-    def split_mime(mime):
+    def clean_mime(mime):
         """
-        Split a mime string into type and subtype: 'text/html; charset=utf-8' -> ('text', 'html')
+        Remove attributes from a mime string: 'text/html; charset=utf-8' -> 'text/html'
         """
         separator = mime.find(';')
         if separator > 0:
-            mime = mime[:separator].strip()
-        parts = mime.split("/")
-        if len(parts) != 2:
-            return None
-        return parts[0], parts[1]
+            mime = mime[:separator]
+        return mime.strip()
 
     def process(self, reply, render_options):
         content_type = reply.header(QNetworkRequest.ContentTypeHeader)
         if not content_type.isValid():
             return
 
-        mimetype = self.split_mime(str(content_type.toString()))
+        mimetype = self.clean_mime(str(content_type.toString()))
         if mimetype is None:
             return
 
         allowed = render_options.get_allowed_content_types()
         forbidden = render_options.get_forbidden_content_types()
-        whitelist = set(map(ContentTypeMiddleware.split_mime, allowed))
-        blacklist = set(map(ContentTypeMiddleware.split_mime, forbidden))
+        whitelist = set(map(ContentTypeMiddleware.clean_mime, allowed))
+        blacklist = set(map(ContentTypeMiddleware.clean_mime, forbidden))
 
         if self.contains(blacklist, mimetype) or not self.contains(whitelist, mimetype):
             if self.verbosity >= 2:
