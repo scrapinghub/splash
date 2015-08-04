e9f00dedf5c640891977101d9affcacf71815189
Alexander Chekunkov <chekunkov@gmail.com>
2015-08-04 18:41:22 +0300

    add test cases for JPEG method in lua emulation and proxy server tests

--- splash/tests/test_execute_emulation.py
+++ splash/tests/test_execute_emulation.py
@@ -85,5 +85,34 @@ server because of OOM killer or grinding user system to a halt because of swap.
         self.assertEqual(alpha_channel.getextrema(), (0, 0))
 
 
+class EmulatedRenderJpegTest(Base.EmulationMixin, test_render.RenderJpegTest):
+    script = 'main = require("emulation").render_jpeg'
+
+    @pytest.mark.xfail(
+        run=False,
+        reason="""
+Range validation in lua renderer is not implemented and out of range values of
+width/height will consume huge amount of memory either bringing down the test
+server because of OOM killer or grinding user system to a halt because of swap.
+""")
+    def test_range_checks(self):
+        super(EmulatedRenderJpegTest, self).test_range_checks()
+
+    def test_extra_height_doesnt_leave_garbage_when_using_tiled_render(self):
+        # XXX: this function belongs to test_render, BUT height < 1000 is fixed
+        # in defaults and so is tile max size, so in order to force rendering
+        # that may produce extra pixels at the bottom we go the way that avoids
+        # parameter validation.
+        r = self.request({'url': self.mockurl('tall'), 'viewport': '100x100',
+                          'height': 3000})
+        img = self.assertJpeg(r, height=3000)
+        # Ensure that the extra pixels at the bottom are transparent.
+        box = img.crop((0, 100, 100, 3000))
+        self.assertEqual(box.size, (100, 2900))
+        # iterate over channels
+        for i in range(3):
+            self.assertEqual(box.getdata(i).getextrema(), (255, 255))
+
+
 class EmulatedRenderHarTest(Base.EmulationMixin, test_har.HarRenderTest):
     script = 'main = require("emulation").render_har'
