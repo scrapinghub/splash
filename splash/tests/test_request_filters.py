99c100fbc6c8f1f77d6c84730326ea6e892ac512
Mikhail Korobov <kmike84@gmail.com>
2016-03-04 05:44:52 +0500

    TST failing test for #399

--- splash/tests/test_request_filters.py
+++ splash/tests/test_request_filters.py
@@ -2,7 +2,10 @@
 from __future__ import absolute_import
 import os
 import shutil
+
 import requests
+import pytest
+
 from splash.tests.utils import TestServers, SplashServer
 from splash.tests.test_render import BaseRenderTest
 
@@ -54,10 +57,19 @@ class FiltersTestHTML(BaseFiltersTest):
         self.assertStatusCode(r, 400)
         self.assertIn('foo', r.text)
 
-    def test_dont_filter_main_request(self):
+    def test_dont_filter_main_request_with_domain_option(self):
         r = self.request({
             'url': self.mockurl('iframes/script.js'),
             'filters': 'noscript'
+        })
+        self.assertStatusCode(r, 200)
+        self.assertIn('document.write', r.text)
+
+    @pytest.mark.xfail(reason="See https://github.com/scrapinghub/splash/issues/399")
+    def test_dont_filter_main_request_without_domain_option(self):
+        r = self.request({
+            'url': self.mockurl('iframes/script2.js'),
+            'filters': 'noscript2'
         })
         self.assertStatusCode(r, 200)
         self.assertIn('document.write', r.text)
