3a6b38a2cbe4ec2c17fc493eb7776f8e17ab0217
Mikhail Korobov <kmike84@gmail.com>
2016-03-07 23:45:16 +0500

    bump Pillow to 3.1.1

--- dockerfiles/splash/provision.sh
+++ dockerfiles/splash/provision.sh
@@ -144,7 +144,7 @@ install_python_deps () {
         adblockparser==0.5 \
         xvfbwrapper==0.2.5 \
         funcparserlib==0.3.6 \
-        Pillow==3.1.0 \
+        Pillow==3.1.1 \
         cython==0.23.4 && \
     ${_PYTHON} -m pip install --install-option="--with-cython" https://github.com/scoder/lupa/archive/8c74d0a278b45df5d813abbab64377e80864bd56.zip#egg=lupa && \
     ${_PYTHON} -m pip install https://github.com/sunu/pyre2/archive/c610be52c3b5379b257d56fc0669d022fd70082a.zip#egg=re2
