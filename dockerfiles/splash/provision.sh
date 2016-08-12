66e3af00d162821b9853da5b6bcf642c5131c2e1
Mikhail Korobov <kmike84@gmail.com>
2016-08-12 10:22:31 +0500

    upgrade Pillow (trying to fix CI)

--- dockerfiles/splash/provision.sh
+++ dockerfiles/splash/provision.sh
@@ -145,7 +145,7 @@ install_python_deps () {
         adblockparser==0.5 \
         xvfbwrapper==0.2.8 \
         funcparserlib==0.3.6 \
-        Pillow==3.2.0 \
+        Pillow==3.3.0 \
         lupa==1.3 && \
     ${_PYTHON} -m pip install https://github.com/sunu/pyre2/archive/c610be52c3b5379b257d56fc0669d022fd70082a.zip#egg=re2
 }
