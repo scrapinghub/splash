7cc606d05425b0e48674c8a4488083943b9cb987
Dennis Fischer <chaosking0912@gmail.com>
2016-10-14 12:09:15 +0200

    Changed jquery resources to respect location protocol

--- splash/resources.py
+++ splash/resources.py
@@ -470,8 +470,8 @@ class DemoUI(_ValidatingResource):
 
             <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/%(theme)s/bootstrap.min.css" rel="stylesheet">
             <link rel="icon" type="image/x-icon" href="/_ui/favicon.ico">
-            <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-            <script src="https://code.jquery.com/jquery-migrate-1.2.1.js"></script>
+            <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+            <script src="//code.jquery.com/jquery-migrate-1.2.1.js"></script>
 
             <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
             %(cm_resources)s
