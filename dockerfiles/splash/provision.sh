c98d671f4e8cd8df4110c977582b5009c307b3d1
Mikhail Korobov <kmike84@gmail.com>
2016-11-11 00:00:49 +0500

    upgrade Pillow to 3.4.2

--- dockerfiles/splash/provision.sh
+++ dockerfiles/splash/provision.sh
@@ -145,7 +145,7 @@ install_python_deps () {
         adblockparser==0.7 \
         xvfbwrapper==0.2.8 \
         funcparserlib==0.3.6 \
-        Pillow==3.3.3 \
+        Pillow==3.4.2 \
         lupa==1.3 && \
     ${_PYTHON} -m pip install https://github.com/sunu/pyre2/archive/c610be52c3b5379b257d56fc0669d022fd70082a.zip#egg=re2
 }
