104c113f651b05cc7eec34d877173c03797b7ec2
Mikhail Korobov <kmike84@gmail.com>
2017-03-03 16:35:01 +0500

    update psutil and pillow in Docker image

--- dockerfiles/splash/provision.sh
+++ dockerfiles/splash/provision.sh
@@ -140,12 +140,12 @@ install_python_deps () {
     ${_PYTHON} -m pip install -U pip && \
     ${_PYTHON} -m pip install \
         qt5reactor==0.3 \
-        psutil==5.0.0 \
+        psutil==5.1.3 \
         Twisted==16.1.1 \
         adblockparser==0.7 \
         xvfbwrapper==0.2.8 \
         funcparserlib==0.3.6 \
-        Pillow==3.4.2 \
+        Pillow==4.0.0 \
         lupa==1.3 && \
     ${_PYTHON} -m pip install https://github.com/sunu/pyre2/archive/c610be52c3b5379b257d56fc0669d022fd70082a.zip#egg=re2
 }
