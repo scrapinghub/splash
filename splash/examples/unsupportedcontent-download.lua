0a6be68c1f1e1cea6f57f4576de1eb8144807166
Mohamed Abdel Maksoud <mohamed@amaksoud.com>
2017-07-03 13:43:21 +0200

    improve the example

--- splash/examples/unsupportedcontent-download.lua
+++ splash/examples/unsupportedcontent-download.lua
@@ -2,8 +2,11 @@ function main(splash)
   splash.unsupported_content = 'download'
   splash.download_directory="/tmp"
   assert(splash:go("http://orimi.com/pdf-test.pdf"))
+  assert(splash:wait(0.5))
   return {
-      png=splash:png(),
+      png = splash:png(),
+      html = splash:html(),
+      har = splash:har(),
       download = splash:download(),
       download_filename = splash:download_filename(),
   }
