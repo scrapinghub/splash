1364e40fa37a2db8b61f33b33555368b7c62407b
woutut <woutut@gmail.com>
2019-12-24 11:36:22 +0200

    [docs] Update method syntax (`.` syntax provide errors).

--- splash/examples/render-multiple.lua
+++ splash/examples/render-multiple.lua
@@ -1,6 +1,6 @@
 function main(splash, args)
-  splash.set_viewport_size(800, 600)
-  splash.set_user_agent('Splash bot')
+  splash:set_viewport_size(800, 600)
+  splash:set_user_agent('Splash bot')
   local example_urls = {"www.google.com", "www.bbc.co.uk", "scrapinghub.com"}
   local urls = args.urls or example_urls
   local results = {}
