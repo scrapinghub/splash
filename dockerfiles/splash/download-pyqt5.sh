d0c116013a73b8b8aaaa284b972985f1985c6c4c
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2020-05-29 09:28:41 +0000

    we have to pin URLs here

--- dockerfiles/splash/download-pyqt5.sh
+++ dockerfiles/splash/download-pyqt5.sh
@@ -2,8 +2,8 @@
 # XXX: these URLs needs to be replaced with sourceforge in future,
 # because riverbank tend to remove old releases.
 SIP="https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz"
-PYQT="https://www.riverbankcomputing.com/static/Downloads/PyQt${QT_MAJOR_VERSION}/${QT_FULL_VERSION}/PyQt${QT_MAJOR_VERSION}_gpl-${QT_FULL_VERSION}.tar.gz"
-WEBENGINE="https://www.riverbankcomputing.com/static/Downloads/PyQtWebEngine/${QT_FULL_VERSION}/PyQtWebEngine_gpl-${QT_FULL_VERSION}.tar.gz"
+PYQT="https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.13.2/PyQt5-5.13.2.tar.gz"
+WEBENGINE="https://www.riverbankcomputing.com/static/Downloads/PyQtWebEngine/5.13.2/PyQtWebEngine-5.13.2.tar.gz"
 
 curl -L -o "$1" ${SIP} && \
 curl -L -o "$2" ${PYQT} && \
