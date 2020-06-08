f3db870036c52d503c061e197d9635246947efed
Ian Duffy <1243435+imduffy15@users.noreply.github.com>
2020-06-08 17:26:59 +0100

    Update dockerfiles/splash/download-pyqt5.sh
    Co-authored-by: Mikhail Korobov <kmike84@gmail.com>

--- dockerfiles/splash/download-pyqt5.sh
+++ dockerfiles/splash/download-pyqt5.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# XXX: these URLs needs to be replaced with sourceforge in future,
+# XXX: riverbankcomputing URLs need to be replaced with files.pythonhosted.org in future,
 # because riverbank tend to remove old releases.
 SIP="https://www.riverbankcomputing.com/static/Downloads/sip/4.19.22/sip-4.19.22.tar.gz"
 PYQT="https://files.pythonhosted.org/packages/4d/81/b9a66a28fb9a7bbeb60e266f06ebc4703e7e42b99e3609bf1b58ddd232b9/PyQt5-5.14.2.tar.gz"
