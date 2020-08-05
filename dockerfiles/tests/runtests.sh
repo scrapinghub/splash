176860949f02a217f105e49cd104dfeb04ff14a1
Adrián Chaves <adrian@chaves.io>
2020-08-05 11:01:25 +0200

    Allow overriding the tests to execute with ‘docker run’ parameters

--- dockerfiles/tests/runtests.sh
+++ dockerfiles/tests/runtests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-py.test --cov=splash --doctest-modules --durations=50 splash "$@" && \
+py.test --cov=splash --doctest-modules --durations=50 "$@" && \
 if [ -n "${TRAVIS}" ]; then
     codecov
 fi;
