21374f4a75c1820185fb7829d74955e11fcfd338
Alexander Sibiryakov <sixty-one@yandex.ru>
2020-09-04 15:48:16 +0200

    pyqt locations update

--- dockerfiles/splash/download-pyqt5.sh
+++ dockerfiles/splash/download-pyqt5.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # XXX: these URLs needs to be replaced with sourceforge in future,
 # because riverbank tend to remove old releases.
-SIP="https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz"
-PYQT="https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.13.2/PyQt5-5.13.2.tar.gz"
-WEBENGINE="https://www.riverbankcomputing.com/static/Downloads/PyQtWebEngine/5.13.2/PyQtWebEngine-5.13.2.tar.gz"
+SIP="https://www.riverbankcomputing.com/static/Downloads/sip/4.19.22/sip-4.19.22.tar.gz"
+PYQT="https://files.pythonhosted.org/packages/4d/81/b9a66a28fb9a7bbeb60e266f06ebc4703e7e42b99e3609bf1b58ddd232b9/PyQt5-5.14.2.tar.gz"
+WEBENGINE="https://files.pythonhosted.org/packages/47/9f/60e630711fd1dd14ef3bd95c86c733c86b8c0853749c7a03691f681f13fd/PyQtWebEngine-5.14.0.tar.gz"
 
 curl --fail -L -o "$1" ${SIP} && \
 curl --fail -L -o "$2" ${PYQT} && \
