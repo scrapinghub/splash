4e8a962970e43cc58a7a36371f4c531d18a23b3f
Alexander Sibiryakov <sixty-one@yandex.ru>
2020-09-07 14:39:10 +0200

    using exact same versions as in prod

--- dockerfiles/splash/download-pyqt5.sh
+++ dockerfiles/splash/download-pyqt5.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # XXX: these URLs needs to be replaced with sourceforge in future,
 # because riverbank tend to remove old releases.
-SIP="https://www.riverbankcomputing.com/static/Downloads/sip/4.19.24/sip-4.19.24.tar.gz"
-PYQT="https://files.pythonhosted.org/packages/4d/81/b9a66a28fb9a7bbeb60e266f06ebc4703e7e42b99e3609bf1b58ddd232b9/PyQt5-5.14.2.tar.gz"
-WEBENGINE="https://files.pythonhosted.org/packages/47/9f/60e630711fd1dd14ef3bd95c86c733c86b8c0853749c7a03691f681f13fd/PyQtWebEngine-5.14.0.tar.gz"
+SIP="https://distfiles.macports.org/py-sip/sip-4.19.19.tar.gz"
+PYQT="https://sources.voidlinux.org/python-PyQt5-5.13.2/PyQt5-5.13.2.tar.gz"
+WEBENGINE="https://sources.voidlinux-ppc.org/python-PyQt5-webengine-5.13.2/PyQtWebEngine-5.13.2.tar.gz"
 
 curl --fail -L -o "$1" ${SIP} && \
 curl --fail -L -o "$2" ${PYQT} && \
